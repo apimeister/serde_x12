2fabcaed23a0c706458b0aac2f828c8426c490d6
Jens Walter <jens@apimeister.com>
2022-05-27 11:33:37 +0200

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 0.2.0 2022-05-27
+
+* make logging optional
+* update readme
+
 # 0.1.0 2022-05-26
 
 * initial version
--- src/ser.rs
+++ src/ser.rs
@@ -1,17 +1,15 @@
-use serde::{ser, Serialize};
 use crate::error::Error;
+use serde::{ser, Serialize};
 
 pub struct Serializer {
     output: Vec<Vec<Option<String>>>,
 }
 
-pub fn to_string<T>(value: &T) -> Result<String,Error>
+pub fn to_string<T>(value: &T) -> Result<String, Error>
 where
     T: Serialize,
 {
-    let mut serializer = Serializer {
-        output: vec![],
-    };
+    let mut serializer = Serializer { output: vec![] };
     value.serialize(&mut serializer)?;
     let mut final_string = "".to_string();
     for line in serializer.output {
@@ -24,7 +22,7 @@ where
         for item in x {
             if item.is_none() {
                 length_of_line -= 1;
-            }else{
+            } else {
                 break;
             }
         }
@@ -36,7 +34,7 @@ where
                 Some(s) => final_string.push_str(&s),
                 None => final_string.push_str(""),
             }
-            final_string.push_str("*");
+            final_string.push('*');
         }
         final_string.pop();
         final_string.push_str("~\n");
@@ -48,11 +46,12 @@ impl<'a> ser::Serializer for &'a mut Serializer {
     type Ok = ();
     type Error = Error;
     fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
+        #[cfg(feature = "debug")]
         println!("serialize_str: {}", v);
         match self.output.last_mut() {
             Some(el) => {
                 el.push(Some(v.to_string()));
-            },
+            }
             None => {
                 self.output.push(vec![Some(v.to_string())]);
             }
@@ -60,6 +59,7 @@ impl<'a> ser::Serializer for &'a mut Serializer {
         Ok(())
     }
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
+        #[cfg(feature = "debug")]
         println!("serialize_none");
         match self.output.last_mut() {
             Some(el) => el.push(None),
@@ -78,51 +78,52 @@ impl<'a> ser::Serializer for &'a mut Serializer {
     type SerializeStruct = Self;
     type SerializeStructVariant = Self;
 
-    fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
+    fn serialize_bool(self, _v: bool) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
+    fn serialize_i8(self, _v: i8) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
+    fn serialize_i16(self, _v: i16) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
+    fn serialize_i32(self, _v: i32) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
+    fn serialize_i64(self, _v: i64) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
+    fn serialize_u8(self, _v: u8) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
+    fn serialize_u16(self, _v: u16) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
+    fn serialize_u32(self, _v: u32) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
+    fn serialize_u64(self, _v: u64) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
+    fn serialize_f32(self, _v: f32) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
-    fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
+    fn serialize_f64(self, _v: f64) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
     fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
+        #[cfg(feature = "debug")]
         println!("serialize_char: {}", v);
         match self.output.last_mut() {
             Some(el) => el.push(Some(v.to_string())),
@@ -133,18 +134,21 @@ impl<'a> ser::Serializer for &'a mut Serializer {
         Ok(())
     }
 
-    fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
+    fn serialize_bytes(self, _v: &[u8]) -> Result<Self::Ok, Self::Error> {
         todo!()
     }
 
     fn serialize_some<T: ?Sized>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
-        T: Serialize {
-            println!("serialize_some");
-            value.serialize(self)
+        T: Serialize,
+    {
+        #[cfg(feature = "debug")]
+        println!("serialize_some");
+        value.serialize(self)
     }
 
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
+        #[cfg(feature = "debug")]
         println!("serialize_unit");
         //empty value
         match self.output.last_mut() {
@@ -156,8 +160,9 @@ impl<'a> ser::Serializer for &'a mut Serializer {
         Ok(())
     }
 
-    fn serialize_unit_struct(self, name: &'static str) -> Result<Self::Ok, Self::Error> {
-        println!("serialize_unit_struct: {}", name);
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
+        #[cfg(feature = "debug")]
+        println!("serialize_unit_struct: {}", _name);
         match self.output.last_mut() {
             Some(el) => el.push(Some("".to_string())),
             None => {
@@ -169,77 +174,90 @@ impl<'a> ser::Serializer for &'a mut Serializer {
 
     fn serialize_unit_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
+        _name: &'static str,
+        _variant_index: u32,
         variant: &'static str,
     ) -> Result<Self::Ok, Self::Error> {
-        println!("serialize_unit_variant: {} {} {}", name, variant_index, variant);
+        #[cfg(feature = "debug")]
+        println!(
+            "serialize_unit_variant: {} {} {}",
+            _name, _variant_index, variant
+        );
         self.serialize_str(variant)
     }
 
     fn serialize_newtype_struct<T: ?Sized>(
         self,
-        name: &'static str,
-        value: &T,
+        _name: &'static str,
+        _value: &T,
     ) -> Result<Self::Ok, Self::Error>
     where
-        T: Serialize {
+        T: Serialize,
+    {
         todo!()
     }
 
     fn serialize_newtype_variant<T: ?Sized>(
         self,
-        name: &'static str,
-        variant_index: u32,
-        variant: &'static str,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
         value: &T,
     ) -> Result<Self::Ok, Self::Error>
     where
-        T: Serialize {
-            println!("serialize_newtype_variant: {} {} {}", name, variant_index, variant);
-            value.serialize(&mut *self)?;
-            Ok(())
+        T: Serialize,
+    {
+        #[cfg(feature = "debug")]
+        println!(
+            "serialize_newtype_variant: {} {} {}",
+            _name, _variant_index, _variant
+        );
+        value.serialize(&mut *self)?;
+        Ok(())
     }
 
-    fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
-        println!("serialize_seq: {:?}",len);
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
+        #[cfg(feature = "debug")]
+        println!("serialize_seq: {:?}", _len);
         Ok(self)
     }
 
-    fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
-        println!("serialize_tuple: {}", len);
+    fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple, Self::Error> {
+        #[cfg(feature = "debug")]
+        println!("serialize_tuple: {}", _len);
         Ok(self)
     }
 
     fn serialize_tuple_struct(
         self,
-        name: &'static str,
-        len: usize,
+        _name: &'static str,
+        _len: usize,
     ) -> Result<Self::SerializeTupleStruct, Self::Error> {
         todo!()
     }
 
     fn serialize_tuple_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
-        variant: &'static str,
-        len: usize,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
     ) -> Result<Self::SerializeTupleVariant, Self::Error> {
         todo!()
     }
 
-    fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
-        println!("serialize_map: {:?}",len);
+    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
+        #[cfg(feature = "debug")]
+        println!("serialize_map: {:?}", _len);
         Ok(self)
     }
 
     fn serialize_struct_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
-        variant: &'static str,
-        len: usize,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
     ) -> Result<Self::SerializeStructVariant, Self::Error> {
         todo!()
     }
@@ -247,9 +265,10 @@ impl<'a> ser::Serializer for &'a mut Serializer {
     fn serialize_struct(
         self,
         name: &'static str,
-        len: usize,
+        _len: usize,
     ) -> Result<Self::SerializeStruct, Self::Error> {
-        println!("serialize_struct: {} {}", name,len);
+        #[cfg(feature = "debug")]
+        println!("serialize_struct: {} {}", name, _len);
         self.output.push(vec![Some(name.to_string())]);
         Ok(self)
     }
@@ -262,12 +281,15 @@ impl<'a> ser::SerializeSeq for &'a mut Serializer {
 
     fn serialize_element<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
     where
-        T: Serialize {
+        T: Serialize,
+    {
+        #[cfg(feature = "debug")]
         println!("SerializeSeq::serialize_element");
         value.serialize(&mut **self)
     }
 
     fn end(self) -> Result<Self::Ok, Self::Error> {
+        #[cfg(feature = "debug")]
         println!("SerializeSeq::end");
         Ok(())
     }
@@ -279,12 +301,15 @@ impl<'a> ser::SerializeTuple for &'a mut Serializer {
 
     fn serialize_element<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
     where
-        T: Serialize {
-            println!("SerializeTuple::serialize_element");
-            value.serialize(&mut **self)
+        T: Serialize,
+    {
+        #[cfg(feature = "debug")]
+        println!("SerializeTuple::serialize_element");
+        value.serialize(&mut **self)
     }
 
     fn end(self) -> Result<Self::Ok, Self::Error> {
+        #[cfg(feature = "debug")]
         println!("SerializeTuple::end");
         Ok(())
     }
@@ -294,9 +319,10 @@ impl<'a> ser::SerializeTupleStruct for &'a mut Serializer {
     type Ok = ();
     type Error = Error;
 
-    fn serialize_field<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
+    fn serialize_field<T: ?Sized>(&mut self, _value: &T) -> Result<(), Self::Error>
     where
-        T: Serialize {
+        T: Serialize,
+    {
         todo!()
     }
 
@@ -309,9 +335,10 @@ impl<'a> ser::SerializeTupleVariant for &'a mut Serializer {
     type Ok = ();
     type Error = Error;
 
-    fn serialize_field<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
+    fn serialize_field<T: ?Sized>(&mut self, _value: &T) -> Result<(), Self::Error>
     where
-        T: Serialize {
+        T: Serialize,
+    {
         todo!()
     }
 
@@ -326,19 +353,24 @@ impl<'a> ser::SerializeMap for &'a mut Serializer {
 
     fn serialize_key<T: ?Sized>(&mut self, key: &T) -> Result<(), Self::Error>
     where
-        T: Serialize {
-            println!("SerializeMap::serialize_key");
-            key.serialize(&mut **self)
+        T: Serialize,
+    {
+        #[cfg(feature = "debug")]
+        println!("SerializeMap::serialize_key");
+        key.serialize(&mut **self)
     }
 
     fn serialize_value<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
     where
-        T: Serialize {
-            println!("SerializeMap::serialize_value");
-            value.serialize(&mut **self)
+        T: Serialize,
+    {
+        #[cfg(feature = "debug")]
+        println!("SerializeMap::serialize_value");
+        value.serialize(&mut **self)
     }
 
     fn end(self) -> Result<Self::Ok, Self::Error> {
+        #[cfg(feature = "debug")]
         println!("SerializeMap::end");
         Ok(())
     }
@@ -350,14 +382,16 @@ impl<'a> ser::SerializeStruct for &'a mut Serializer {
 
     fn serialize_field<T: ?Sized>(
         &mut self,
-        key: &'static str,
+        _key: &'static str,
         value: &T,
     ) -> Result<(), Self::Error>
     where
-        T: Serialize {
-            println!("serialize_field: {}", key);
-            // self.output.push(vec![key.to_string()]);
-            value.serialize(&mut **self);
+        T: Serialize,
+    {
+        #[cfg(feature = "debug")]
+        println!("serialize_field: {}", _key);
+        // self.output.push(vec![key.to_string()]);
+        let _ = value.serialize(&mut **self);
         Ok(())
     }
 
@@ -373,11 +407,12 @@ impl<'a> ser::SerializeStructVariant for &'a mut Serializer {
 
     fn serialize_field<T: ?Sized>(
         &mut self,
-        key: &'static str,
-        value: &T,
+        _key: &'static str,
+        _value: &T,
     ) -> Result<(), Self::Error>
     where
-        T: Serialize {
+        T: Serialize,
+    {
         todo!()
     }
 
